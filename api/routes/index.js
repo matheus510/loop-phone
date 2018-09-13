a067dd81366d6338b376803287901642ddaafdaa
Matheus Fonseca <matheus510.fonseca@gmail.com>
2018-09-13 09:56:03 -0300

    setting up simple server

---
+++ api/routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * GET v1/status
+ */
+router.get('/status', (req, res) => res.send('OK'))
+
+export default router
---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+// import morgan from 'morgan'
+import cors from 'cors'
+import routes from './api/routes/'
+
+process.env.PORT = 5000
+
+const app = express()
+
+// Parse body params and attach them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+// Set api routes
+app.use('/', routes)
+
+app.listen(process.env.PORT, () => { console.log(`Listening on port:${process.env.PORT}`) })
+
+export default app
